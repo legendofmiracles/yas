381c5715e9e338736dc93655c8a99dfaa328f003
ur name <legendofmiracles@protonmail.com>
2020-09-15 21:28:31 +0000

    asks the user 3 times for a password now

--- src/hash.rs
+++ src/hash.rs
@@ -1,14 +1,16 @@
 use sha2::Digest;
 use std::fs;
-
+use std::io::Write;
+use users::{get_current_uid, get_user_by_uid};
+// #[derive(Copy)]
 struct Hash {
     format: u8,
     hash: String,
     salt: String,
-    password: String,
 }
 
-pub fn check_passwd(pwd: String, user: users::User) {
+pub fn check_passwd() -> bool {
+    let user = get_user_by_uid(get_current_uid()).unwrap();
     let contents: String = fs::read_to_string("/etc/shadow")
         .expect("yas: error when reading from /etc/shadow file (╯°□°）╯︵ ┻━┻");
     // iterate over each line of the file and filter it by the username of the invoking user.
@@ -25,22 +27,32 @@ pub fn check_passwd(pwd: String, user: users::User) {
         format: hash_non_struct[1].parse().unwrap(),
         hash: hash_non_struct[3].to_string(),
         salt: hash_non_struct[2].to_string(),
-        password: pwd.replace("\n", ""),
     };
-    let is_match = match hash_struct.format {
-        1 => todo!(),
-        2 => todo!(),
-        3 => todo!(),
-        4 => todo!(),
-        5 => todo!(),
-        6 => sha256(hash_struct),
-        _ => panic!("unknown encryption method (╯°□°）╯︵ ┻━┻"),
-    };
+
+    for i in 0..3 {
+        let pwd = ask_pass(user.name().to_str().unwrap());
+        let is_match = match hash_struct.format {
+            1 => todo!(),
+            2 => todo!(),
+            3 => todo!(),
+            4 => todo!(),
+            5 => todo!(),
+            6 => sha256(&hash_struct, pwd),
+            _ => panic!("unknown encryption method (╯°□°）╯︵ ┻━┻"),
+        };
+        if is_match {
+            return true;
+        } else if i != 3 {
+            eprintln!("yas: wrong password. Nice try.");
+        }
+    }
+    eprintln!("yas: three wrong passwords. Quitting...");
+    return false;
 }
 
-fn sha256(hash_struct: Hash) -> bool {
+fn sha256(hash_struct: &Hash, password: String) -> bool {
     let mut hasher = sha2::Sha256::new();
-    let mut hash_with_salt = hash_struct.password;
+    let mut hash_with_salt = password;
     hash_with_salt.push_str(&hash_struct.salt);
     hasher.update(&hash_with_salt.as_bytes());
     let final_res = hasher.finalize();
@@ -50,4 +62,16 @@ fn sha256(hash_struct: Hash) -> bool {
     } else {
         false
     }
+}
+
+fn ask_pass(user: &str) -> String {
+    let mut input = String::new();
+    print!("yas: password for user {}: ", user);
+    std::io::stdout()
+        .flush()
+        .expect("yas: unable to flush stdout (╯°□°）╯︵ ┻━┻");
+    std::io::stdin()
+        .read_line(&mut input)
+        .expect("error: unable to read user input (╯°□°）╯︵ ┻━┻");
+    input
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 use std::os::unix::process::CommandExt;
 use std::process::Command;
 mod hash;
-use std::io::Write;
-use users::{get_current_uid, get_user_by_uid};
 fn main() {
-    let user = get_user_by_uid(get_current_uid()).unwrap();
-    hash::check_passwd(ask_pass(user.name().to_str().unwrap()), user);
+    hash::check_passwd();
     let mut args: Vec<String> = std::env::args().collect();
     args.remove(0);
     // if the command runs sucessfully, this program will immediately quit.
@@ -16,16 +13,4 @@ fn main() {
         .raw_os_error();
     let error = std::io::Error::from_raw_os_error(command.unwrap());
     println!("yas: {} (╯°□°）╯︵ ┻━┻", error);
-}
-
-fn ask_pass(user: &str) -> String {
-    let mut input = String::new();
-    print!("yas: password for user {}: ", user);
-    std::io::stdout()
-        .flush()
-        .expect("yas: unable to flush stdout (╯°□°）╯︵ ┻━┻");
-    std::io::stdin()
-        .read_line(&mut input)
-        .expect("error: unable to read user input (╯°□°）╯︵ ┻━┻");
-    input
 }
