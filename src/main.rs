245f82fb9195a6a1c86a78bde58444948e7a0b7c
ur name <legendofmiracles@protonmail.com>
2020-09-16 06:45:23 +0000

    exits on three wrong passwords, and only lets you run the command, when you entered the correct password

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,20 @@ use std::os::unix::process::CommandExt;
 use std::process::Command;
 mod hash;
 fn main() {
-    hash::check_passwd();
     let mut args: Vec<String> = std::env::args().collect();
     args.remove(0);
+    let matches: bool = hash::check_passwd();
+    if matches {
+        // this function will either immediately quit the program (as seen in the comments below), or it will inform the user of a error and then quit,
+        // thus making returning a error incredibly stupid
+        do_the_actual_thing(args);
+    } else {
+        // We exit here, because the bool only gets returned as false, if we had three wrong password inputs.
+        std::process::exit(1);
+    }
+}
+
+fn do_the_actual_thing(mut args: Vec<String>) {
     // if the command runs sucessfully, this program will immediately quit.
     // Otherwise the program will inform the user that it didn't start perfectly fine
     let command = Command::new(args.remove(0))
